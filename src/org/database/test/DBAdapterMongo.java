1d6ca58d6a8c7f1240170114c12a6760f56809a1
Alex <xv500i@gmail.com>
2013-05-03 11:01:20 +0200

    mongo inserts

--- src/org/database/test/DBAdapterMongo.java
+++ src/org/database/test/DBAdapterMongo.java
@@ -6,7 +6,6 @@ package org.database.test;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
-import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import java.net.UnknownHostException;
@@ -49,7 +48,6 @@ public class DBAdapterMongo extends AbstractDBAdapter {
     
     MongoClient mc;
     DB db;
-    DBCollection tpcCollection;
     
     @Override
     public void connect() {
@@ -60,9 +58,6 @@ public class DBAdapterMongo extends AbstractDBAdapter {
             System.exit(-1);
         }
         db = mc.getDB("test");
-        // Clean the DB
-        tpcCollection = db.getCollection("tpc");
-        tpcCollection.drop();
     }
 
     @Override
@@ -73,32 +68,137 @@ public class DBAdapterMongo extends AbstractDBAdapter {
 
     @Override
     protected void firstInsertOperation() {
-        List<DBObject> inserts = new ArrayList<>();
-        int i;
-        // 666 part
-        for (i = 0; i < 666; i++) {
+        List<DBObject> inserts;
+        // 5 regions
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
             BasicDBObject part = new BasicDBObject();
-            // TODO: attributes here
+            part.append("_id", i);// [1-5]
+            part.append("R_name", GenerationUtility.generateString(32));
+            part.append("R_comment", GenerationUtility.generateString(80));
             inserts.add(part);
         }
+        db.getCollection("region").insert(inserts);
         
-        // 2666 partsupps
-        // 20000 lineitems
-        // 5000 orders
-        // 500 customers
-        // 33 supplier
         // 25 nations
-        // 5 regions
-        tpcCollection.insert(inserts);
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 25; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", i); // [1-25]
+            part.append("a", GenerationUtility.generateString(32));
+            part.append("b", (i%5)+1);
+            part.append("c", GenerationUtility.generateString(32));
+        }
+        db.getCollection("nation").insert(inserts);
+        
+        // 666 part
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 666; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", i); // [1-666]
+            part.append("a", GenerationUtility.generateString(32));
+            part.append("b", GenerationUtility.generateString(32));
+            part.append("c", GenerationUtility.generateString(32));
+            part.append("d", GenerationUtility.generateString(32));
+            part.append("e", GenerationUtility.generateInteger());
+            part.append("f", GenerationUtility.generateString(32));
+            part.append("g", GenerationUtility.generateNumber(6,2));
+            part.append("h", GenerationUtility.generateString(32));
+        }
+        db.getCollection("part").insert(inserts);
+        
+        // 33 supplier
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 33; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", i); // [1-33]
+            part.append("a", GenerationUtility.generateString(32));
+            part.append("b", GenerationUtility.generateString(32));
+            part.append("c", (i%25)+1);
+            part.append("d", GenerationUtility.generateString(32));
+            part.append("e", GenerationUtility.generateNumber(6, 2));
+            part.append("f", GenerationUtility.generateString(52));
+        }
+        db.getCollection("supplier").insert(inserts);
+
+        // 500 customers
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 500; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", i); // [1-500]
+            part.append("a", GenerationUtility.generateString(32));
+            part.append("b", GenerationUtility.generateString(32));
+            part.append("c", (i%25)+1);
+            part.append("d", GenerationUtility.generateString(32));
+            part.append("e", GenerationUtility.generateNumber(6, 2));
+            part.append("f", GenerationUtility.generateString(32));
+            part.append("g", GenerationUtility.generateString(60));
+        }
+        db.getCollection("customer").insert(inserts);
+
+        // 2666 partsupps
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 2666; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("a", (i%666)+1);
+            part.append("b", (i%33)+1);
+            part.append("c", GenerationUtility.generateInteger());
+            part.append("d", GenerationUtility.generateNumber(6, 2));
+            part.append("f", GenerationUtility.generateString(100));
+        }
+        db.getCollection("partsupp").insert(inserts);
+
+        // 5000 orders
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 5000; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", i); // [1-5000]
+            part.append("a", (i%500)+1);
+            part.append("b", GenerationUtility.generateString(32));
+            part.append("c", GenerationUtility.generateNumber(6, 2));
+            part.append("d", GenerationUtility.generateDate());
+            part.append("e", GenerationUtility.generateString(32));
+            part.append("f", GenerationUtility.generateString(32));
+            part.append("g", GenerationUtility.generateInteger());
+            part.append("h", GenerationUtility.generateString(40));
+        }
+        db.getCollection("order").insert(inserts);
+        
+        // 20000 lineitems
+        inserts = new ArrayList<>();
+        for (int i = 1; i <= 20000; i++) {
+            BasicDBObject part = new BasicDBObject();
+            part.append("_id", (i%5000)+1);
+            part.append("a", (i%666)+1);
+            part.append("b", (i%33)+1);
+            part.append("c", GenerationUtility.generateInteger());
+            part.append("d", GenerationUtility.generateInteger());
+            part.append("e", GenerationUtility.generateNumber(6, 2));
+            part.append("f", GenerationUtility.generateNumber(6, 2));
+            part.append("g", GenerationUtility.generateNumber(6, 2));
+            part.append("h", GenerationUtility.generateString(32));
+            part.append("i", GenerationUtility.generateString(32));
+            part.append("j", GenerationUtility.generateDate());
+            part.append("k", GenerationUtility.generateDate());
+            part.append("l", GenerationUtility.generateDate());
+            part.append("m", GenerationUtility.generateString(32));
+            part.append("n", GenerationUtility.generateString(32));
+            part.append("o", GenerationUtility.generateString(32));
+        }
+        db.getCollection("lineitem").insert(inserts);
+            
     }
 
     @Override
     protected void secondInsertOperation() {
+        // C&P pero sumant a i +20000
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     public void doQuery1() {
+        //DBCursor find = tpcCollection.find(new BasicDBObject());
+        //System.out.println("Query 1 returned: " + find.count() + " rows.");
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
--- src/org/database/test/DBAdapterOracle.java
+++ src/org/database/test/DBAdapterOracle.java
@@ -5,7 +5,6 @@
 package org.database.test;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
