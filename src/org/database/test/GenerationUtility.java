44f0dabe7f111118092bdbcdea601524a14cb0d5
natxoraga22 <natxoraga22@gmail.com>
2013-05-04 13:34:26 +0200

    GenerationUtility millorada amb possibles NULLs

--- src/org/database/test/GenerationUtility.java
+++ src/org/database/test/GenerationUtility.java
@@ -16,7 +16,11 @@ public class GenerationUtility {
     /**
      * @return an integer with at least 4 digits 
      */
-    public static int generateInteger() {
+    public static Integer generateInteger() {
+        // NULL value?
+        int nullLimit = r.nextInt(10);
+        if (nullLimit == 0) return null;
+        
         int integer;
         do {
             integer = r.nextInt();
@@ -28,6 +32,10 @@ public class GenerationUtility {
      * @return a random date 
      */
     public static Date generateDate() {
+        // NULL value?
+        int nullLimit = r.nextInt(10);
+        if (nullLimit == 0) return null;
+        
         return new Date(r.nextLong());
     }
     
@@ -36,6 +44,10 @@ public class GenerationUtility {
      * @return a string of size 'size' with characters [A-Z, a-z]
      */
     public static String generateString(int size) {
+        // NULL value?
+        int nullLimit = r.nextInt(10);
+        if (nullLimit == 0) return null;
+        
         StringBuilder sb = new StringBuilder(size);
         for (int i = 0; i < size; i++) {
             char c = (char) ((r.nextBoolean() ? 'A' : 'a') + r.nextInt('z' - 'a'));
@@ -49,8 +61,12 @@ public class GenerationUtility {
      * @param decimalDigits the number of decimal digits wanted
      * @return a double with numberOfDigits digits and decimalDigits decimal digits
      */
-    public static double generateNumber(int numberOfDigits, int decimalDigits) {
+    public static Double generateNumber(int numberOfDigits, int decimalDigits) {
         // example 7,2: [0, 9999,99]
+        // NULL value?
+        int nullLimit = r.nextInt(10);
+        if (nullLimit == 0) return null;
+        
         double digits = (int)(Math.pow(10d, numberOfDigits) * r.nextDouble() - 1.0d);
         return digits / Math.pow(10.0d, decimalDigits);
     }
