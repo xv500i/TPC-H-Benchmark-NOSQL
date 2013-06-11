b75d650c4ff85557a2df72ef940adaae05869149
natxoraga22 <natxoraga22@gmail.com>
2013-06-12 01:25:31 +0200

    Classes Neo4j creades

---
+++ src/org/database/test/DBAdapterNeo4j.java
@@ -0,0 +1,49 @@
+
+package org.database.test;
+
+/**
+ * @author Natxo Raga Llorens
+ */
+public class DBAdapterNeo4j extends AbstractDBAdapter {
+
+    @Override
+    public void connect() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void disconnect() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void firstInsertOperation() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void secondInsertOperation() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doQuery1() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doQuery2() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doQuery3() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void doQuery4() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
