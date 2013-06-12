a5333ef9b323547b660d2156b057acf5a5dcc735
natxoraga22 <natxoraga22@gmail.com>
2013-06-12 02:19:49 +0200

    Conexió i desconexió

--- src/org/database/test/DBAdapterNeo4j.java
+++ src/org/database/test/DBAdapterNeo4j.java
@@ -1,19 +1,44 @@
 
 package org.database.test;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+
 /**
  * @author Natxo Raga Llorens
  */
 public class DBAdapterNeo4j extends AbstractDBAdapter {
 
+    private final static String DB_PATH = "neo4j";
+    
+    private GraphDatabaseService graphDB;
+    
+    
+    // Shutdown Hook
+    private static void registerShutdownHook(final GraphDatabaseService graphDB) {
+        // Registers a shutdown hook for the Neo4j instance so that it shuts down 
+        // nicely when the VM exits (even if you "Ctrl-C" the running application).
+        Runtime.getRuntime().addShutdownHook(new Thread()
+            {
+                @Override
+                public void run()
+                {
+                    graphDB.shutdown();
+                }
+            } 
+        );
+    }
+    
     @Override
     public void connect() {
-        throw new UnsupportedOperationException("Not supported yet.");
+         graphDB = new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH);
+         registerShutdownHook(graphDB);
     }
 
     @Override
     public void disconnect() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        graphDB.shutdown();
     }
 
     @Override
